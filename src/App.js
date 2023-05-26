0bf403d34da0727e4f012bd8009a1c34f0189a2f
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2023-05-25 21:43:19 -0600

    Logica de Firebase movida a App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+
 import Header from './components/Header';
 import Chat from './components/Chat';
 import SignIn from './components/SignIn';
@@ -10,6 +15,62 @@ function App() {
   const [userName, setUserName] = useState('Nombre del Usuario')
   const [profilePicUrl , setProfilePicUrl] = useState(null)
 
+      // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyCm67pLWeZX7cM60I6ysT_VNj22o2LsIKs",
+      authDomain: "projecto-ujed.firebaseapp.com",
+      projectId: "projecto-ujed",
+      storageBucket: "projecto-ujed.appspot.com",
+      messagingSenderId: "409314775966",
+      appId: "1:409314775966:web:ef0fad292b123f8922bc8f",
+      measurementId: "G-20CE6NL28F"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const analytics = getAnalytics(app);
+    const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUserName(currentUser.displayName)
+        setLogIn(true)
+        setProfilePicUrl(currentUser.photoURL)
+    })
+
+    function logInHandler() {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                console.log(user)
+                
+                if(user.email !== undefined) {
+                    setUserName(result.user.displayName)
+                    setLogIn(true)
+                    setProfilePicUrl(user.photoURL)
+                }
+                //console.log(user)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+
+    }
+
   if(logIn) {
     return (
       <div className="App">
@@ -20,7 +81,7 @@ function App() {
   } else {
     return (
       <div className="App">
-        <SignIn setUserName={setUserName} setLogIn={setLogIn} setProfilePicUrl={setProfilePicUrl}/>
+        <SignIn logInHandler={logInHandler} setUserName={setUserName} setLogIn={setLogIn} setProfilePicUrl={setProfilePicUrl}/>
       </div>
     );
   }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,69 +1,9 @@
 import ujedLogo from '../assets/img/ujed-logo-1.png'
 import './css/SignIn.css'
 import {FaGoogle} from 'react-icons/fa'
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 
 
 function SignIn(props) {
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyCm67pLWeZX7cM60I6ysT_VNj22o2LsIKs",
-      authDomain: "projecto-ujed.firebaseapp.com",
-      projectId: "projecto-ujed",
-      storageBucket: "projecto-ujed.appspot.com",
-      messagingSenderId: "409314775966",
-      appId: "1:409314775966:web:ef0fad292b123f8922bc8f",
-      measurementId: "G-20CE6NL28F"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth();
-    const analytics = getAnalytics(app);
-    const provider = new GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-
-    onAuthStateChanged(auth, (currentUser) => {
-        props.setUserName(currentUser.displayName)
-        props.setLogIn(true)
-        props.setProfilePicUrl(currentUser.photoURL)
-    })
-
-    function logIn() {
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-                // IdP data available using getAdditionalUserInfo(result)
-                console.log(user)
-                
-                if(user.email !== undefined) {
-                    props.setUserName(result.user.displayName)
-                    props.setLogIn(true)
-                    props.setProfilePicUrl(user.photoURL)
-                }
-                //console.log(user)
-                // ...
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.customData.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-            });
-
-    }
-
     return (
         <article className="signin-screen">
             <header className="signin-screen__header">
@@ -79,7 +19,7 @@ function SignIn(props) {
                 </div>
             </article>
             <section className='signin-screen__form'>
-                <div className='signin-button' onClick={logIn}>
+                <div className='signin-button' onClick={props.logInHandler}>
                     <span><FaGoogle /></span>
                     <span>iniciar sesion</span>
                 </div>
